24d65f65b3855d555116abe2124d2f47519f06eb
TCAA Admin <info@tcaausa.com>
2013-07-24 03:17:03 +0000

    override title viewlet to use registry setting instead of context page title

--- tcaa/content/viewlets/viewlets.py
+++ tcaa/content/viewlets/viewlets.py
@@ -1,12 +1,14 @@
-
+from cgi import escape
 
 from five import grok
 from Acquisition import aq_inner
-
-from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getMultiAdapter
 from plone.app.layout.viewlets.interfaces import IHtmlHead
+from Products.CMFPlone.utils import safe_unicode
 
 from tcaa.content.interfaces import ITCAAContentish
+from tcaa.content.controlpanel import TCAASettings
+
 
 class TCAADublinCoreViewlet(grok.Viewlet):
 
@@ -19,9 +21,30 @@ class TCAADublinCoreViewlet(grok.Viewlet):
         context = aq_inner(self.context)
         self.metatags = []
         if hasattr(context, 'metaTitle'):
-            self.metatags.append({'name':'title','content':context.metaTitle})
+            self.metatags.append({'name': 'title',
+                                  'content': context.metaTitle})
         if hasattr(context, 'metaDescription'):
-            self.metatags.append({'name':'description','content':context.metaDescription})
+            self.metatags.append({'name': 'description',
+                                  'content': context.metaDescription})
         if hasattr(context, 'metaKeywords'):
-            self.metatags.append({'name':'keywords','content':context.metaKeywords})
+            self.metatags.append({'name': 'keywords',
+                                  'content': context.metaKeywords})
 
+
+class TCAATitleViewlet(grok.Viewlet):
+    grok.name('plone.htmlhead.title')
+    grok.context(ITCAAContentish)
+    grok.require('zope2.View')
+    grok.viewletmanager(IHtmlHead)
+
+    def update(self):
+        portal_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_portal_state')
+        portalTitle = escape(safe_unicode(portal_state.navigation_root_title()))
+        pageTitle = self._currentTitle()
+        self.site_title = u"%s &mdash; %s" % (pageTitle, portalTitle)
+
+    def _currentTitle(self):
+        settings = TCAASettings()
+        title = settings.forName('html_title', '')
+        return title
